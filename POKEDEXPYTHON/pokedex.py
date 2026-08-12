a37399b25437257c14154a1fbd1ca26a8a2ef69c
AlessandroErick-prog <alessandro.sousa@escolar.ifrn.edu.br>
2026-08-12 18:57:11 +0000

    pokedex 12/08

---
+++ POKEDEXPYTHON/pokedex.py
@@ -0,0 +1,22 @@
+print("Bem vindo à sua Pokedéx de Bolso!")
+
+nome = input("Qual é o seu nome do seu pokémon?: ")
+
+#len - comprimento
+
+quantidade_letras = len(nome)
+
+print(f"O nome do seu pokémon tem {quantidade_letras} letras")
+
+tipo = input("Qual é o tipo do seu pokémon (ex: Fogo, Água, Elétrico)?: ")
+
+hp_pokemon = int(input("Qual é o HP (vida) do seu pokémon?: "))
+
+peso_pokemon = float(input("Qual é o peso do seu pokémon?: "))
+
+print("RELATÓRIO FINAL")
+print(f"Nome do pokémon": nome)
+print(f"Tipo do pokémon": tipo)
+print(f"HP do pokémon": hp_pokemon)
+print(f"Peso do pokémon": peso_pokemon kg)
+
